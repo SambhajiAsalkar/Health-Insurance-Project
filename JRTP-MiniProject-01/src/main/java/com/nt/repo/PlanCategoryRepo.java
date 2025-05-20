f1bbab93366074ffed1d3c0b07a77e0006210e84
sambhajiasalkar <sambhajiasalkar850@gmail.com>
2025-05-20 13:52:04 +0530

    only related modules

--- JRTP-MiniProject-01/src/main/java/com/nt/repo/PlanCategoryRepo.java
+++
@@ -1,9 +0,0 @@
-package com.nt.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.nt.entity.PlanCategory;
-
-public interface PlanCategoryRepo extends JpaRepository<PlanCategory, Integer> {
-
-}
--- JRTP-MultipleDBConnections/src/main/java/com/nt/repository/prod/IProductRepo.java
+++
@@ -1,9 +0,0 @@
-package com.nt.repository.prod;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.nt.model.prod.Product;
-
-public interface IProductRepo extends JpaRepository<Product,Integer>{
-
-}
